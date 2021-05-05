0cbeef2cfa654261bf30bd410bf0a3ab7dd2fc2d
Danilo <danilodegortes@gmail.com>
2021-05-05 09:07:54 +0200

    install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { IBeaconRegion, IBeaconAdvertisement } from 'capacitor-ibeacon';
+const { IBeacon } = Plugins;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
@@ -15,4 +18,22 @@ export class AppComponent {
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
+
+  async ngOnInit() {
+    await IBeacon.requestAlwaysAuthorization();
+    IBeacon.addListener('enteredRegion', (response) => {
+      const region = response.region;
+      console.log(`Entered region: ${region.identifier} (${region.uuid})`);
+    });
+    IBeacon.addListener('leftRegion', (response) => {
+      const region = response.region;
+      console.log(`Left region: ${region.identifier} (${region.uuid})`);
+    });
+    IBeacon.startMonitoringForRegion({
+      uuid: 'DEADBEEF-0000-0000-0000-000000000000',
+      identifier: 'Dead Beef',
+    });
+  }
+
+
 }
